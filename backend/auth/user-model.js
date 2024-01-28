fc280f0e70f54d9ebaa51048f010867f7808a8f7
MONISHKUMAR LR <106217981+lrmonish@users.noreply.github.com>
2024-01-28 18:37:26 +0530

    Update user-model.js

--- backend/auth/user-model.js
+++ backend/auth/user-model.js
@@ -10,7 +10,6 @@ const userSchema = mongoose.Schema({
 userSchema.pre('save', async function (next) {
     if (this.isModified('password')) {
       this.password = await bcrypt.hash(this.password, 8);
-      console.log(this.password);
     }
     next();
   });
